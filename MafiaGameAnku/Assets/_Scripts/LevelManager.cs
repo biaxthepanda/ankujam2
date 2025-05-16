9e7c97e214de85bee02d4ebbf9c07291514c138a
biaxthepanda <biaxgamestudios@gmail.com>
2025-05-16 21:33:34 +0300

    setup

---
+++ MafiaGameAnku/Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance { get; private set; }
+
+    private void Awake()
+    {
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    public int DayIndex;
+
+}
